267adbd22b0ab65a8e82fdcb54a1dd0b427af946
Sevdimali Isayev <github853211>
2021-08-02 06:51:30 +0400

    variable

---
+++ cond_variable/variable_cond.py
@@ -0,0 +1,31 @@
+import time
+from threading import Thread, Condition
+
+
+class StingySpendy:
+    money = 100
+    cv = Condition()
+
+    def stingy(self):
+        for i in range(1000000):
+            self.cv.acquire()
+            self.money += 10
+            self.cv.notify()
+            self.cv.release()
+        print("Stingy Done")
+
+    def spendy(self):
+        for i in range(500000):
+            self.cv.acquire()
+            self.money -= 20
+            while self.money < 20:
+                self.cv.wait()
+            self.cv.release()
+        print("Spendy Done")
+
+
+ss = StingySpendy()
+Thread(target=ss.stingy, args=()).start()
+Thread(target=ss.spendy, args=()).start()
+time.sleep(5)
+print("Money in the end", ss.money)
